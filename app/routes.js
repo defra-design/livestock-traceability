8c4d8256a96865f144566d7e20e7d7074c842a79
james-lee-oliver <james.oliver@defra.gov.uk>
2026-05-01 14:09:30 +0100

    Routes fixes

--- app/routes.js
+++ app/routes.js
@@ -8,107 +8,107 @@ const router = govukPrototypeKit.requests.setupRouter()
 
 // SGD routes
 
-router.post('/version1/sign-in-account', function (request, response) {
+router.post('/movements/version1/sign-in-account', function (request, response) {
 
   var signIn = request.session.data['signIn']
   if (signIn == "login") {
-    response.redirect("/version1/sign-in-account")
+    response.redirect("/movements/version1/sign-in-account")
   } else {
-    response.redirect("/version1b/select-animal")
+    response.redirect("/movements/version1b/select-animal")
   }
 })
 
-router.post('/version1/recently-used', function (request, response) {
+router.post('/movements/version1/recently-used', function (request, response) {
 
   var destination = request.session.data['destination']
   if (destination == "recent") {
-    response.redirect("/version1/recently-used")
+    response.redirect("/movements/version1/recently-used")
   }
   else if (destination == "cph") {
-    response.redirect("/version1/cph-search")
+    response.redirect("/movements/version1/cph-search")
   }
   else {
-    response.redirect("/version1b/manual-search")
+    response.redirect("/movements/version1b/manual-search")
   }
 })
 
-router.post('/version1/bluetooth-tag-reader', function (request, response) {
+router.post('/movements/version1/bluetooth-tag-reader', function (request, response) {
 
   var count = request.session.data['count']
   if (count == "scan") {
-    response.redirect("/version1/bluetooth-tag-reader")
+    response.redirect("/movements/version1/bluetooth-tag-reader")
   }
   else if (count == "batch") {
-    response.redirect("/version1b/batch")
+    response.redirect("/movements/version1b/batch")
   }
   else {
-    response.redirect("/version1b/individual-tags")
+    response.redirect("/movements/version1b/individual-tags")
   }
 })
 
-router.post('/version1/check-your-answers', function (request, response) {
+router.post('/movements/version1/check-your-answers', function (request, response) {
 
   var transport = request.session.data['transport']
   if (transport == "self") {
-    response.redirect("/version1/check-your-answers")
+    response.redirect("/movements/version1/check-your-answers")
   }
   else {
-    response.redirect("/version1/helper-choice")
+    response.redirect("/movements/version1/helper-choice")
   }
 })
 
-router.post('/version1b/self-move-details', function (request, response) {
+router.post('/movements/version1b/self-move-details', function (request, response) {
 
   var transportb = request.session.data['transportb']
   if (transportb == "self") {
-    response.redirect("/version1b/self-move-details")
+    response.redirect("/movements/version1b/self-move-details")
   }
   else {
-    response.redirect("/version1b/helper-choice")
+    response.redirect("/movements/version1b/helper-choice")
   }
 })
 
-router.post('/version2/check-recipient-move', function (request, response) {
+router.post('/movements/version2/check-recipient-move', function (request, response) {
 
   var submitted = request.session.data['submitted']
   if (submitted == "submitted") {
-    response.redirect("/version2/check-recipient-move")
+    response.redirect("/movements/version2/check-recipient-move")
   }
   else {
-    response.redirect("/version2/movement-date")
+    response.redirect("/movements/version2/movement-date")
   }
 })
 
-router.post('/version3/check-recipient-move', function (request, response) {
+router.post('/movements/version3/check-recipient-move', function (request, response) {
 
   var submitted = request.session.data['submitted']
   if (submitted == "submitted") {
-    response.redirect("/version3/check-recipient-move")
+    response.redirect("/movements/version3/check-recipient-move")
   }
   else {
-    response.redirect("/version3/movement-from")
+    response.redirect("/movements/version3/movement-from")
   }
 })
 
-router.post('/version3/select-vehicle', function (request, response) {
+router.post('/movements/version3/select-vehicle', function (request, response) {
 
   var submitted = request.session.data['transport']
   if (submitted == "self") {
-    response.redirect("/version3/select-vehicle")
+    response.redirect("/movements/version3/select-vehicle")
   }
   else {
-    response.redirect("/version3/haulier-detail-choice")
+    response.redirect("/movements/version3/haulier-detail-choice")
   }
 })
 
-router.post('/version3/helper-details', function (request, response) {
+router.post('/movements/version3/helper-details', function (request, response) {
 
   var submitted = request.session.data['haulier']
   if (submitted == "yes") {
-    response.redirect("/version3/helper-details")
+    response.redirect("/movements/version3/helper-details")
   }
   else {
-    response.redirect("/version3/check-your-answers-minus-haulier")
+    response.redirect("/movements/version3/check-your-answers-minus-haulier")
   }
 })
 
